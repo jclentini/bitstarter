3794ed7800db1121327c24d30cd9676be9c7d54c
jclentini <jcl@lentini.net>
2013-07-10 20:41:18 +0000

    Modified File

--- web.js
+++ web.js
@@ -4,9 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  	fs.readFile(__dirname + 'index.html', 'utf8', function(err, text){
-	response.send(text));
-	});
+	response.send(fs.readFileSync(__dirname + 'index.html, 'utf8');
+
 });
 
 var port = process.env.PORT || 5000;
